4431a06a7ef3463e1fb9eff4d12877285e82ad9c
whoisham <ham.uygun@gmail.com>
2026-06-28 11:58:15 +0200

    US-01 : add player movement

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,29 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+
+public class Main {
+    static void main(String[] sender) {
+        Board board = new Board();
+        Scanner scanner = new Scanner(System.in);
+        char currentPlayer = 'X';
+
+        for (int turn = 0; turn < 3; turn++) {
+            System.out.println("Current Player: " + currentPlayer);
+            board.printBoard();
+
+            System.out.print("row (0-2): ");
+            int row = scanner.nextInt();
+            System.out.print("column (0-2): ");
+            int col = scanner.nextInt();
+
+            if (board.makeMove(row, col, currentPlayer)) {
+                currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+            } else {
+                System.out.println("Invalid move! Try again.");
+                turn--;
+            }
         }
     }
 }
